66f7d4fff36b974ce97ee21866c06eb866771bea
adriennk146 <aboboo146@gmail.com>
2020-10-15 18:56:23 +0200

    Plusz feladatok befejezve, kisebb finomítások a UI-n.

--- app/src/main/java/hu/adri/kopapirollo/MainActivity.java
+++ app/src/main/java/hu/adri/kopapirollo/MainActivity.java
@@ -17,17 +17,17 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView user,gep;
-    private TextView userScoreOut,gepScoreOut,dontetlen;
-    private Button koBtn,papirBtn,olloBtn;
+    private ImageView user, gep, userE1, userE2, userE3, gepE1, gepE2, gepE3;
+    private TextView dontetlen;
+    private Button koBtn, papirBtn, olloBtn;
     private Random r;
     private int gepValasztott;
     private int userChoiceValue;
-    private int userScore;
-    private int gepScore;
     private AlertDialog alert;
     private AlertDialog.Builder builder;
     private int dontetlenDb;
+    private int userElet;
+    private int gepElet;
 
 
     @Override
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 user.setImageResource(R.drawable.rock);
 
-                userChoiceValue=1;
+                userChoiceValue = 1;
                 Valasztott();
             }
         });
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 user.setImageResource(R.drawable.paper);
 
-                userChoiceValue=2;
+                userChoiceValue = 2;
                 Valasztott();
             }
         });
@@ -61,40 +61,46 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 user.setImageResource(R.drawable.scissors);
 
-                userChoiceValue=3;
+                userChoiceValue = 3;
                 Valasztott();
             }
         });
     }
 
-    private void init(){
-        this.user=findViewById(R.id.userChoice);
-        this.gep=findViewById(R.id.botChoice);
-        this.userScoreOut=findViewById(R.id.emberScore);
-        this.gepScoreOut=findViewById(R.id.gepScore);
-        this.koBtn=findViewById(R.id.koBtn);
-        this.papirBtn=findViewById(R.id.papirBtn);
-        this.olloBtn=findViewById(R.id.olloBtn);
-        this.r=new Random();
-        this.gepValasztott=r.nextInt(2)+1;
-        this.userScore=0;
-        this.gepScore=0;
-        dontetlen=findViewById(R.id.dontetlenDb);
-        dontetlenDb=0;
-        builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setMessage("Szeretnél új játékot?")
+    private void init() {
+        this.userE1 = findViewById(R.id.emberH1);
+        this.userE2 = findViewById(R.id.emberH2);
+        this.userE3 = findViewById(R.id.emberH3);
+        this.gepE1 = findViewById(R.id.gepH1);
+        this.gepE2 = findViewById(R.id.gepH2);
+        this.gepE3 = findViewById(R.id.gepH3);
+        this.user = findViewById(R.id.userChoice);
+        this.gep = findViewById(R.id.botChoice);
+        this.koBtn = findViewById(R.id.koBtn);
+        this.papirBtn = findViewById(R.id.papirBtn);
+        this.olloBtn = findViewById(R.id.olloBtn);
+        this.r = new Random();
+        this.userElet = 3;
+        this.gepElet = 3;
+        this.dontetlen = findViewById(R.id.dontetlenDb);
+        this.dontetlenDb = 0;
+        this.builder = new AlertDialog.Builder(MainActivity.this);
+        this.builder.setMessage("Szeretnél új játékot?")
                 .setPositiveButton("Igen", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        gepValasztott=r.nextInt(2)+1;
-                        userScore=0;
-                        gepScore=0;
-                        dontetlenDb=0;
+                        dontetlenDb = 0;
+                        userElet = 3;
+                        gepElet = 3;
                         dontetlen.setText("0");
                         user.setImageResource(R.drawable.rock);
                         gep.setImageResource(R.drawable.rock);
-                        userScoreOut.setText("0");
-                        gepScoreOut.setText("0");
+                        userE1.setImageResource(R.drawable.heart2);
+                        userE2.setImageResource(R.drawable.heart2);
+                        userE3.setImageResource(R.drawable.heart2);
+                        gepE1.setImageResource(R.drawable.heart2);
+                        gepE2.setImageResource(R.drawable.heart2);
+                        gepE3.setImageResource(R.drawable.heart2);
                     }
                 })
                 .setNegativeButton("Nem", new DialogInterface.OnClickListener() {
@@ -109,103 +115,102 @@ public class MainActivity extends AppCompatActivity {
         alert = builder.create();
     }
 
-    private void Valasztott(){
-        switch(gepValasztott){
-            case 1: gep.setImageResource(R.drawable.rock);break;
-            case 2: gep.setImageResource(R.drawable.paper); break;
-            case 3: gep.setImageResource(R.drawable.scissors);break;
-            default: break;
+    private void Valasztott() {
+        gepValasztott = r.nextInt(2) + 1;
+        switch (gepValasztott) {
+            case 1:
+                gep.setImageResource(R.drawable.rock);
+                break;
+            case 2:
+                gep.setImageResource(R.drawable.paper);
+                break;
+            case 3:
+                gep.setImageResource(R.drawable.scissors);
+                break;
+            default:
+                break;
         }
         KiNyert();
     }
 
-    private void KiNyert(){
-        if(userChoiceValue==1)
-        {
-            if(gepValasztott==1)
-            {
+    private void KiNyert() {
+        if (userChoiceValue == 1) {
+            if (gepValasztott == 1) {
                 Toast.makeText(this, "Döntetlen!", Toast.LENGTH_LONG).show();
                 dontetlenDb++;
-                dontetlen.setText(dontetlenDb+"");
+                dontetlen.setText(dontetlenDb + "");
 
-            }
-            else if(gepValasztott==2)
-            {
+            } else if (gepValasztott == 2) {
                 Toast.makeText(this, "A gép nyert!", Toast.LENGTH_LONG).show();
-                gepScore++;
-                gepScoreOut.setText(gepScore+"");
-                if(gepScore>=3){
-                    JatekVege();
-                }
-            }
-            else{
+                EletMinuszUser();
+            } else {
                 Toast.makeText(this, "Te nyertél!", Toast.LENGTH_LONG).show();
-                userScore++;
-                userScoreOut.setText(userScore+"");
-                if(userScore>=3 ){
-                    JatekVege();
-                }
+                EletMinuszGep();
             }
-        }
-        else if(userChoiceValue==2)
-        {
-            if(gepValasztott==1)
-            {
+        } else if (userChoiceValue == 2) {
+            if (gepValasztott == 1) {
                 Toast.makeText(this, "Te nyertél!", Toast.LENGTH_LONG).show();
-                userScore++;
-                userScoreOut.setText(userScore+"");
-                if(userScore>=3){
-                    JatekVege();
-                }
-            }
-            else if(gepValasztott==2)
-            {
+                EletMinuszGep();
+            } else if (gepValasztott == 2) {
                 Toast.makeText(this, "Döntetlen!", Toast.LENGTH_LONG).show();
                 dontetlenDb++;
-                dontetlen.setText(dontetlenDb+"");
-            }
-            else{
+                dontetlen.setText(dontetlenDb + "");
+            } else {
                 Toast.makeText(this, "A gép nyert!", Toast.LENGTH_LONG).show();
-                gepScore++;
-                gepScoreOut.setText(gepScore+"");
-                if(gepScore>=3){
-                    JatekVege();
-                }
+                EletMinuszUser();
             }
-        }
-        else{
-            if(gepValasztott==1)
-            {
+        } else {
+            if (gepValasztott == 1) {
                 Toast.makeText(this, "A gép nyert!", Toast.LENGTH_LONG).show();
-                gepScore++;
-                gepScoreOut.setText(gepScore+"");
-                if(gepScore>=3){
-                    JatekVege();
-                }
-
-
-            }
-            else if(gepValasztott==2)
-            {
+                EletMinuszUser();
+            } else if (gepValasztott == 2) {
                 Toast.makeText(this, "Te nyertél!", Toast.LENGTH_LONG).show();
-                userScore++;
-                userScoreOut.setText(userScore+"");
-                if(userScore>=3){
-                    JatekVege();
-                }
-            }
-            else{
+                EletMinuszGep();
+            } else {
                 Toast.makeText(this, "Döntetlen!", Toast.LENGTH_LONG).show();
                 dontetlenDb++;
-                dontetlen.setText(dontetlenDb+"");
+                dontetlen.setText(dontetlenDb + "");
             }
         }
-        gepValasztott=r.nextInt(2)+1;
     }
 
-    private void JatekVege(){
+    private void EletMinuszGep() {
+        switch (gepElet) {
+            case 3:
+                gepE3.setImageResource(R.drawable.heart1);
+                break;
+            case 2:
+                gepE2.setImageResource(R.drawable.heart1);
+                break;
+            case 1:
+                gepE1.setImageResource(R.drawable.heart1);
+                break;
+        }
+        gepElet--;
+        if (gepElet == 0) {
+            JatekVege();
+        }
+    }
+
+    private void EletMinuszUser() {
+        switch (userElet) {
+            case 3:
+                userE3.setImageResource(R.drawable.heart1);
+                break;
+            case 2:
+                userE2.setImageResource(R.drawable.heart1);
+                break;
+            case 1:
+                userE1.setImageResource(R.drawable.heart1);
+                break;
+        }
+        userElet--;
+        if (userElet == 0) {
+            JatekVege();
+        }
+    }
+
+    private void JatekVege() {
         alert.show();
     }
-
-
 }
